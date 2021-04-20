1d66c58e54abedd1462cd83d17ecd0855100f41c
Isabella18081996 <Isabella.Nita@yahoo.it>
2021-04-20 16:19:32 +0200

    html e css

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $('next').click(nextImage);
+  $('prev').click(prevImage);
+
+
+
+
+
+  function nextImage(){
+    console.log('next')
+  }
+
+  function prevImage(){
+    console.log('prev')
+  }
+})
